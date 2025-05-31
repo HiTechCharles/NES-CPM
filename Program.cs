a82a418d9f341205d713dd1b11da7b7d8fc29d64
HiTechCharles <71556752+HiTechCharles@users.noreply.github.com>
2025-05-31 07:10:55 -0400

    simplification and code cleanup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Speech.Synthesis;
 
@@ -29,7 +28,7 @@ namespace CPM  //Computer Picker for Monopoly, this won't work on apple II or IB
             Law[5] = "Cannot buy orange or red properties.";
             Law[6] = "Can only build on 1 monopoly.";
             Law[7] = "After getting a monopoly, you must pass go 5 times before building on it.";
-            Law[8] = "Play one of the built-in scenarios from the game editor menu.";
+            Law[8] = "Play one of the built-in scenarios from the game editor menu.  (1 to 4 players)";
             Law[9] = "All Players start with $500.";
             Law[10] = "All players start with $2500.";
             Law[11] = "Must keep at least $500 available at all times.";
@@ -75,14 +74,10 @@ namespace CPM  //Computer Picker for Monopoly, this won't work on apple II or IB
         static void Main(string[] args)  
         {
             //store player names
-            PlayerName[0] = "Arthur";
-            PlayerName[1] = "Gertrude";
-            PlayerName[2] = "Erwin";
-            PlayerName[3] = "Maude";
-            PlayerName[4] = "Carmen";
-            PlayerName[5] = "Isaac";
-            PlayerName[6] = "Penelope";
-            PlayerName[7] = "Ollie";
+            PlayerName[0] = "Arthur";   PlayerName[1] = "Gertrude";
+            PlayerName[2] = "Erwin";    PlayerName[3] = "Maude";
+            PlayerName[4] = "Carmen";   PlayerName[5] = "Isaac";
+            PlayerName[6] = "Penelope"; PlayerName[7] = "Ollie";
 
             Console.Title = "Computer Picker for NES Monopoly";  //console title
             Console.ForegroundColor = ConsoleColor.White;  //text color for console
@@ -92,11 +87,12 @@ namespace CPM  //Computer Picker for Monopoly, this won't work on apple II or IB
             Console.WriteLine("the NES version of Monopoly.  All you have");
             Console.WriteLine("to do is supply how many computer players");
             Console.WriteLine("are needed.  The game will determine the");
-            Console.WriteLine("CPU players to use.\n");
+            Console.WriteLine("CPU players to use.  All output from the");
+            Console.WriteLine("program  goes into a log file.");
             
             int NumCPU = GetNumber("How many Computer opponents?  ");  //number of CPU opponents
             Directory.CreateDirectory(LogPath);  //make sure directory exists
-            System.IO.StreamWriter TodayLog = new System.IO.StreamWriter(TodayLogPath);  //open a file for writing
+            StreamWriter TodayLog = new System.IO.StreamWriter(TodayLogPath);  //open a file for writing
             DateTime end = DateTime.Now;  //get current date & time
             
             //writes all choices into a log file
@@ -106,19 +102,17 @@ namespace CPM  //Computer Picker for Monopoly, this won't work on apple II or IB
            
             for (int i = 0; i < NumCPU; i++)  //loop once for each player picked
             {
-                string GP = GetPlayer();  //pick an unchosen opponent
-                TodayLog.Write(GP + ", ");  //and to file
+                TodayLog.Write(GetPlayer() + ", ");  //write player names to file
             }
             TodayLog.WriteLine(); 
-            string GR = GameRule(); //pick a game rule and store it
-            TodayLog.WriteLine("GAME RULE CHOSEN:  " + GR);
+            TodayLog.WriteLine("GAME RULE CHOSEN:  " + GameRule());
             TodayLog.Close();
 
             string TodayLogContents = File.ReadAllText(TodayLogPath);
             SpeechSynthesizer MonoSpeak  = new SpeechSynthesizer();
-            MonoSpeak.Rate = 3;
+            MonoSpeak.Rate = 4;
 
-            Stopwatch MonoTimer = new Stopwatch();
+            Stopwatch MonoTimer = new Stopwatch();  
             Console.WriteLine(TodayLogContents);
             MonoSpeak.SpeakAsync(TodayLogContents);
 
@@ -127,21 +121,17 @@ namespace CPM  //Computer Picker for Monopoly, this won't work on apple II or IB
             Console.Write("TIMER:  STARTED\t\t");
             MonoTimer.Start();
             Console.ReadKey();
-            Console.Write("TIMER:  STOPPED\t\t");
+            Console.Write("TIMER:  STOPPED");
             MonoTimer.Stop();
             TimeSpan elapsed = MonoTimer.Elapsed;
 
-            StreamWriter AddToLog = new System.IO.StreamWriter(TodayLogPath, true);  //open a file for writing
+            StreamWriter AddToLog = new StreamWriter(TodayLogPath, true);  //open a file for writing
             AddToLog.WriteLine($"    Elapsed Time:  {elapsed.Hours} hours, {elapsed.Minutes} minutes, {elapsed.Seconds} seconds");
             AddToLog.WriteLine("\n--------------------------------------------------\n");
             AddToLog.Close();
 
             TodayLogContents = File.ReadAllText(TodayLogPath);
             File.AppendAllText(FullLogPath, TodayLogContents);
-            
-
-
-
         }
     
     } //end class
